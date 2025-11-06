8ef600ba69253cba57088bcb14a54a9ae81bdb25
HafidaAssendal <hafida.assendal@gmail.com>
2025-11-06 10:49:50 +0100

    Entity Message

--- src/main/java/com/chatop/rental/entites/Message.java
+++ src/main/java/com/chatop/rental/entites/Message.java
@@ -1,12 +1,10 @@
 package com.chatop.rental.entites;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity
